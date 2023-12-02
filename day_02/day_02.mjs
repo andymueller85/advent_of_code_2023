95ccc7bf7a773ec4e0b64f6d6546011f20c0c9c5
Andy Mueller <andymueller85@gmail.com>
2023-12-02 08:49:20 -0500

    day 2 part 2

--- day_02/day_02.mjs
+++ day_02/day_02.mjs
@@ -10,6 +10,46 @@ const TOTAL_RED_COUNT = 12
 const TOTAL_GREEN_COUNT = 13
 const TOTAL_BLUE_COUNT = 14
 
+const partB = fileName => {
+  const gameResults = parseInput(fileName).map(game => {
+    let redMin = 0
+    let greenMin = 0
+    let blueMin = 0
+
+    const splitGame = game.split(': ')
+
+    const [_, gameNumber] = splitGame[0].split(' ')
+    const cubeSets = splitGame[1].split('; ')
+
+    cubeSets.forEach(set => {
+      set.split(', ').forEach(cube => {
+        const [count, color] = cube.split(' ')
+        const parsedCount = parseInt(count)
+        switch (color) {
+          case 'red':
+            if (parsedCount > redMin) redMin = parsedCount
+            break
+          case 'green':
+            if (parsedCount > greenMin) greenMin = parsedCount
+            break
+          case 'blue':
+            if (parsedCount > blueMin) blueMin = parsedCount
+            break
+        }
+      })
+    })
+
+    return { gameNumber, redMin, greenMin, blueMin }
+  })
+
+  const ignoreZero = val => (val === 0 ? 1 : val)
+
+  return gameResults.reduce(
+    (acc, cur) => acc + ignoreZero(cur.redMin) * ignoreZero(cur.greenMin) * ignoreZero(cur.blueMin),
+    0
+  )
+}
+
 const partA = fileName => {
   const gameResults = parseInput(fileName).map(game => {
     let possible = true
@@ -25,10 +65,13 @@ const partA = fileName => {
         switch (color) {
           case 'red':
             if (count > TOTAL_RED_COUNT) possible = false
+            break
           case 'green':
             if (count > TOTAL_GREEN_COUNT) possible = false
+            break
           case 'blue':
             if (count > TOTAL_BLUE_COUNT) possible = false
+            break
         }
       })
     })
@@ -51,3 +94,4 @@ const process = (part, expectedAnswer, fn) => {
 }
 
 process('A', 8, partA)
+process('B', 2286, partB)
