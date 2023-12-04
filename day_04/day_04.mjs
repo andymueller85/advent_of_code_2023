f389613760e8d99b63cd13c07059d05c344e9715
Andy Mueller <andymueller85@gmail.com>
2023-12-04 17:39:53 -0500

    day 4 part 2

--- day_04/day_04.mjs
+++ day_04/day_04.mjs
@@ -6,16 +6,42 @@ const parseInput = fileName =>
     .split(/\r?\n/)
     .filter(d => d)
 
+const splitOnWS = str => str.split(/\s/g).filter(s => s !== '')
+
 const partA = fileName =>
   parseInput(fileName).reduce((acc, rawRow) => {
     const [rawCard, rawWinningNumbers] = rawRow.split(' | ')
     const [_, rawTicketNumbers] = rawCard.split(': ')
-    const parsedTicketNumbers = rawTicketNumbers.replaceAll('  ', ' ').split(' ')
-    const parsedWinningNumbers = rawWinningNumbers.replaceAll('  ', ' ').split(' ')
+    const parsedTicketNumbers = splitOnWS(rawTicketNumbers)
+    const parsedWinningNumbers = splitOnWS(rawWinningNumbers)
     const winners = parsedTicketNumbers.filter(num => parsedWinningNumbers.includes(num))
 
     return acc + Math.floor(2 ** (winners.length - 1))
   }, 0)
+
+const partB = fileName => {
+  const rawRows = parseInput(fileName)
+
+  const ticketMap = {}
+  for (let i = 1; i <= rawRows.length; i++) {
+    ticketMap[i] = { numCards: 1, winners: 0 }
+  }
+
+  rawRows.forEach(rawRow => {
+    const [rawCard, rawWinningNumbers] = rawRow.split(' | ')
+    const [rawCardNumber, rawTicketNumbers] = rawCard.split(': ')
+    const parsedTicketNumbers = splitOnWS(rawTicketNumbers)
+    const parsedWinningNumbers = splitOnWS(rawWinningNumbers)
+    const winners = parsedTicketNumbers.filter(num => parsedWinningNumbers.includes(num))
+    const cardNumber = parseInt(splitOnWS(rawCardNumber)[1])
+
+    ticketMap[cardNumber].winners = winners.length
+    for (let i = cardNumber + 1; i <= cardNumber + winners.length; i++) {
+      ticketMap[i].numCards += ticketMap[cardNumber].numCards
+    }
+  })
+  return Object.values(ticketMap).reduce((acc, cur) => acc + cur.numCards, 0)
+}
 
 const process = (part, expectedAnswer, fn) => {
   const sampleAnswer = fn('./day_04/sample_input.txt')
@@ -29,4 +55,4 @@ const process = (part, expectedAnswer, fn) => {
 }
 
 process('A', 13, partA)
-// process('B', 467835, partB)
+process('B', 30, partB)
