79e185adb90f8e74d30e4020563d7356a30fd0b6
Andy Mueller <andymueller85@gmail.com>
2023-12-28 12:50:23 -0500

    day 10 refactor / cleanup

--- day_10/day_10.mjs
+++ day_10/day_10.mjs
@@ -16,161 +16,118 @@ const pipeMap = {
   F: ['S', 'E']
 }
 
+const northNeighbor = (grid, r, c) => (r > 0 ? grid[r - 1][c].pipe : '.')
+const eastNeighbor = (grid, r, c) => (c < grid[r].length - 1 ? grid[r][c + 1].pipe : '.')
+const southNeighbor = (grid, r, c) => (r < grid.length - 1 ? grid[r + 1][c].pipe : '.')
+const westNeighbor = (grid, r, c) => (c > 0 ? grid[r][c - 1].pipe : '.')
+const isConnection = (pipe, dir) => pipeMap[pipe] && pipeMap[pipe].includes(dir)
+
 const findFirstConnection = (grid, r, c) => {
-  const north = r > 0 ? grid[r - 1][c].pipe : '.'
-  if (pipeMap[north] && pipeMap[north].includes('S')) return { row: r - 1, col: c, cameFrom: 'S' }
-  const east = c < grid[r].length - 1 ? grid[r][c + 1].pipe : '.'
-  if (pipeMap[east] && pipeMap[east].includes('W')) return { row: r, col: c + 1, cameFrom: 'W' }
-  const south = r < grid.length - 1 ? grid[r + 1][c].pipe : '.'
-  if (pipeMap[south] && pipeMap[south].includes('N')) return { row: r + 1, col: c, cameFrom: 'N' }
-  const west = c > 0 ? grid[r][c - 1].pipe : '.'
-  if (pipeMap[west] && pipeMap[west].includes('E')) return { row: r, col: c - 1, cameFrom: 'E' }
+  if (isConnection(northNeighbor(grid, r, c), 'S')) return { row: r - 1, col: c, cameFrom: 'S' }
+  if (isConnection(eastNeighbor(grid, r, c), 'W')) return { row: r, col: c + 1, cameFrom: 'W' }
+  if (isConnection(southNeighbor(grid, r, c), 'N')) return { row: r + 1, col: c, cameFrom: 'N' }
+  if (isConnection(westNeighbor(grid, r, c), 'E')) return { row: r, col: c - 1, cameFrom: 'E' }
 }
 
 const getPipeShape = (grid, r, c) => {
   const connections = []
-  const north = r > 0 ? grid[r - 1][c].pipe : '.'
-  if (pipeMap[north] && pipeMap[north].includes('S')) connections.push('N')
-  const east = c < grid[r].length - 1 ? grid[r][c + 1].pipe : '.'
-  if (pipeMap[east] && pipeMap[east].includes('W')) connections.push('E')
-  const south = r < grid.length - 1 ? grid[r + 1][c].pipe : '.'
-  if (pipeMap[south] && pipeMap[south].includes('N')) connections.push('S')
-  const west = c > 0 ? grid[r][c - 1].pipe : '.'
-  if (pipeMap[west] && pipeMap[west].includes('E')) connections.push('W')
+  if (isConnection(northNeighbor(grid, r, c), 'S')) connections.push('N')
+  if (isConnection(eastNeighbor(grid, r, c), 'W')) connections.push('E')
+  if (isConnection(southNeighbor(grid, r, c), 'N')) connections.push('S')
+  if (isConnection(westNeighbor(grid, r, c), 'E')) connections.push('W')
 
   if (connections.length !== 2) throw new Error('ruh roh')
 
   return Object.entries(pipeMap).find(([_, v]) => connections.every(c => v.includes(c)))[0]
 }
 
-const partA = fileName => {
-  const grid = parseInput(fileName)
+const getNorthNodes = (grid, row, col) =>
+  grid.slice(0, row).map((_, rowI, subGrid) => subGrid[rowI][col])
+const getSouthNodes = (grid, row, col) =>
+  grid.slice(row + 1).map((_, rowI, subGrid) => subGrid[rowI][col])
+const getWestNodes = (grid, row, col) => grid[row].slice(0, col)
+const getEastNodes = (grid, row, col) => grid[row].slice(col + 1)
+
+const makeFilterFn =
+  (straightPipe, dir) =>
+  ({ pipe, isInLoop }) =>
+    isInLoop && pipe !== straightPipe && pipeMap[pipe].includes(dir)
+
+const getNSCrossingsCount = nodes =>
+  nodes.filter(({ pipe, isInLoop }) => isInLoop && pipe === '-').length +
+  Math.min(nodes.filter(makeFilterFn('-', 'W')).length, nodes.filter(makeFilterFn('-', 'E')).length)
+
+const getEWCrossingsCount = nodes =>
+  nodes.filter(({ pipe, isInLoop }) => isInLoop && pipe === '|').length +
+  Math.min(nodes.filter(makeFilterFn('|', 'N')).length, nodes.filter(makeFilterFn('|', 'S')).length)
+
+const isOdd = val => val % 2 !== 0
+const isInsideLoop = (grid, row, col) =>
+  !grid[row][col].isInLoop &&
+  isOdd(getNSCrossingsCount(getNorthNodes(grid, row, col))) &&
+  isOdd(getNSCrossingsCount(getSouthNodes(grid, row, col))) &&
+  isOdd(getEWCrossingsCount(getWestNodes(grid, row, col))) &&
+  isOdd(getEWCrossingsCount(getEastNodes(grid, row, col)))
+
+const getStartCoordinates = grid => {
   const startRow = grid.findIndex(r => r.some(({ pipe }) => pipe === 'S'))
-  const startCol = grid[startRow].findIndex(({ pipe }) => pipe === 'S')
-  let { row, col, cameFrom } = findFirstConnection(grid, startRow, startCol)
-  let stepCount = 1
-
-  while (grid[row][col].pipe !== 'S') {
-    let nextDir = pipeMap[grid[row][col].pipe].find(e => e !== cameFrom)
-    if (nextDir === 'N') {
-      row--
-      cameFrom = 'S'
-    } else if (nextDir === 'E') {
-      col++
-      cameFrom = 'W'
-    } else if (nextDir === 'S') {
-      row++
-      cameFrom = 'N'
-    } else if (nextDir === 'W') {
-      col--
-      cameFrom = 'E'
-    }
-    stepCount++
-  }
-
-  return Math.ceil(stepCount / 2)
+  return { startRow, startCol: grid[startRow].findIndex(({ pipe }) => pipe === 'S') }
 }
 
-const getNorthNodes = (grid, row, col) => {
-  return grid.slice(0, row).map((_, rowI, subGrid) => subGrid[rowI][col])
-}
-const getSouthNodes = (grid, row, col) => {
-  return grid.slice(row + 1).map((_, rowI, subGrid) => subGrid[rowI][col])
-}
-const getWestNodes = (grid, row, col) => {
-  return grid[row].slice(0, col)
-}
-const getEastNodes = (grid, row, col) => {
-  return grid[row].slice(col + 1)
-}
-
-const getNSCrossingsCount = nodes => {
-  let crossingsCount = 0
-  crossingsCount += nodes.filter(({ pipe, isInLoop }) => isInLoop && pipe === '-').length
-
-  const westBends = nodes.filter(
-    ({ pipe, isInLoop }) => isInLoop && pipe !== '-' && pipeMap[pipe].includes('W')
-  ).length
-  const eastBends = nodes.filter(
-    ({ pipe, isInLoop }) => isInLoop && pipe !== '-' && pipeMap[pipe].includes('E')
-  ).length
-  crossingsCount += Math.min(westBends, eastBends)
-
-  return crossingsCount
-}
-
-const getEWCrossingsCount = nodes => {
-  let crossingsCount = 0
-  crossingsCount += nodes.filter(({ pipe, isInLoop }) => isInLoop && pipe === '|').length
-
-  const northBends = nodes.filter(
-    ({ pipe, isInLoop }) => isInLoop && pipe !== '|' && pipeMap[pipe].includes('N')
-  ).length
-  const southBends = nodes.filter(
-    ({ pipe, isInLoop }) => isInLoop && pipe !== '|' && pipeMap[pipe].includes('S')
-  ).length
-  crossingsCount += Math.min(northBends, southBends)
-
-  return crossingsCount
-}
-
-const isInsideLoop = (grid, row, col) => {
-  if (grid[row][col].isInLoop) return false
-
-  const northNodes = getNorthNodes(grid, row, col)
-  const northMightBeInLoop = getNSCrossingsCount(northNodes) % 2 !== 0
-
-  const southNodes = getSouthNodes(grid, row, col)
-  const southMightBeInLoop = getNSCrossingsCount(southNodes) % 2 !== 0
-
-  const westNodes = getWestNodes(grid, row, col)
-  const westMightBeInLoop = getEWCrossingsCount(westNodes) % 2 !== 0
-
-  const eastNodes = getEastNodes(grid, row, col)
-  const eastMightBeInLoop = getEWCrossingsCount(eastNodes) % 2 !== 0
-
-  const retval = northMightBeInLoop && southMightBeInLoop && westMightBeInLoop && eastMightBeInLoop
-  return retval
-}
-
-const partB = fileName => {
-  const grid = parseInput(fileName)
-  const startRow = grid.findIndex(r => r.some(({ pipe }) => pipe === 'S'))
-  const startCol = grid[startRow].findIndex(({ pipe }) => pipe === 'S')
-  grid[startRow][startCol].isInLoop = true
-  let { row, col, cameFrom } = findFirstConnection(grid, startRow, startCol)
+const makeLoop = (grid, row, col, cameFrom) => {
   let stepCount = 1
 
   while (grid[row][col].pipe !== 'S') {
     grid[row][col].isInLoop = true
-    let nextDir = pipeMap[grid[row][col].pipe].find(e => e !== cameFrom)
-    if (nextDir === 'N') {
-      row--
-      cameFrom = 'S'
-    } else if (nextDir === 'E') {
-      col++
-      cameFrom = 'W'
-    } else if (nextDir === 'S') {
-      row++
-      cameFrom = 'N'
-    } else if (nextDir === 'W') {
-      col--
-      cameFrom = 'E'
+    switch (pipeMap[grid[row][col].pipe].find(e => e !== cameFrom)) {
+      case 'N':
+        row--
+        cameFrom = 'S'
+        break
+      case 'E':
+        col++
+        cameFrom = 'W'
+        break
+      case 'S':
+        row++
+        cameFrom = 'N'
+        break
+      case 'W':
+        col--
+        cameFrom = 'E'
+        break
     }
     stepCount++
   }
 
   // convert S to pipe
-  grid[startRow][startCol] = getPipeShape(grid, startRow, startCol)
+  grid[row][col] = getPipeShape(grid, row, col)
+  return stepCount
+}
 
-  return grid.reduce((rowAcc, curRow, curRowI) => {
-    return (
+const partA = fileName => {
+  const grid = parseInput(fileName)
+  const { startRow, startCol } = getStartCoordinates(grid)
+  const { row, col, cameFrom } = findFirstConnection(grid, startRow, startCol)
+
+  return Math.ceil(makeLoop(grid, row, col, cameFrom) / 2)
+}
+
+const partB = fileName => {
+  const grid = parseInput(fileName)
+  const { startRow, startCol } = getStartCoordinates(grid)
+  let { row, col, cameFrom } = findFirstConnection(grid, startRow, startCol)
+  makeLoop(grid, row, col, cameFrom)
+
+  return grid.reduce(
+    (rowAcc, curRow, curRowI) =>
       rowAcc +
-      curRow.reduce((colAcc, _, curColI) => {
-        return colAcc + (isInsideLoop(grid, curRowI, curColI) ? 1 : 0)
-      }, 0)
-    )
-  }, 0)
+      curRow.reduce(
+        (colAcc, _, curColI) => colAcc + (isInsideLoop(grid, curRowI, curColI) ? 1 : 0),
+        0
+      ),
+    0
+  )
 }
 
 const process = (part, expectedAnswer, fn, sampleInput) => {
