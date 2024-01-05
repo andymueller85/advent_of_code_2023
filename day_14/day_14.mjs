07e42934a69a4461ccd76a04f294f93c7f4f7933
Andy Mueller <andymueller85@gmail.com>
2024-01-05 09:05:48 -0500

    day 14b (will probs take weeks to finish)

--- day_14/day_14.mjs
+++ day_14/day_14.mjs
@@ -7,32 +7,97 @@ const parseInput = fileName =>
     .filter(d => d)
     .map(r => r.split(''))
 
-const shiftNorth = col => {
-  const cubeIndexes = [-1]
-  col.forEach((a, i) => {
-    if (a === '#') cubeIndexes.push(i)
+const columnLoad = col => {
+  let columnLoad = 0
+
+  const sections = col
+    .join('')
+    .split('#')
+    .map(s => s.split(''))
+
+  sections.forEach((section, sectionI) => {
+    if (section.includes('O')) {
+      const idx = section.findIndex(s => s !== 'O')
+      const stoneCount = idx === -1 ? section.length : idx
+      const firstIndex =
+        sectionI === 0
+          ? 0
+          : sections.reduce((acc, cur, i) => acc + (i < sectionI ? cur.length : 0), 0) + sectionI
+
+      for (let i = 0; i < stoneCount; i++) {
+        columnLoad += col.length - (firstIndex + i)
+      }
+    }
   })
 
-  return cubeIndexes.reduce(
-    (acc, cur, i, arr) =>
-      acc +
-      Array.from(
-        { length: col.slice(Math.max(cur, 0), arr[i + 1]).filter(a => a === 'O').length },
-        (_, i) => i
-      ).reduce((a, b) => a + col.length - (cur + 1) - b, 0),
-    0
-  )
+  return columnLoad
 }
 
-const partA = fileName => {
-  const grid = parseInput(fileName)
-  let totalLoad = 0
+const calculateLoad = grid => {
+  return swapXY(grid).reduce((acc, cur) => {
+    return acc + columnLoad(cur)
+  }, 0)
+}
+
+const swapXY = grid =>
+  Array.from({ length: grid[0].length }, (_, i) => i).map(c => grid.map(r => r[c]))
+
+const roll = col => {
+  const sections = col
+    .join('')
+    .split('#')
+    .map(s => s.split(''))
+
+  const rolledSections = sections.map(s => {
+    const stoneCount = s.filter(a => a === 'O').length
+    return 'O'.repeat(stoneCount) + '.'.repeat(s.length - stoneCount)
+  })
+
+  return rolledSections.join('#').split('')
+}
+
+const rollNorth = grid => {
+  let columns = []
   for (let i = 0; i < grid[0].length; i++) {
-    const columnLoad = shiftNorth(grid.map(r => r[i]))
-    totalLoad += columnLoad
+    columns.push(roll(grid.map(r => r[i])))
   }
 
-  return totalLoad
+  return swapXY(columns)
+}
+
+const rollEast = grid => grid.map(r => roll(r.reverse()).reverse())
+
+const rollSouth = grid => {
+  let columns = []
+  for (let i = 0; i < grid[0].length; i++) {
+    columns.push(roll(grid.map(r => r[i]).reverse()).reverse())
+  }
+
+  return swapXY(columns)
+}
+
+const rollWest = grid => grid.map(roll)
+
+const partA = fileName => calculateLoad(rollNorth(parseInput(fileName)))
+
+const partB = fileName => {
+  let grid = parseInput(fileName)
+
+  for (let i = 0; i < 1000000000; i++) {
+    grid = rollNorth(grid)
+    grid = rollWest(grid)
+    grid = rollSouth(grid)
+    grid = rollEast(grid)
+
+    if (i % 10000000 === 0) {
+      console.log('index', i)
+      grid.forEach(r => console.log(r.join('')))
+      console.log('load', calculateLoad(grid))
+      console.log('\n')
+    }
+  }
+
+  return calculateLoad(grid)
 }
 
 const process = (part, expectedAnswer, fn) => {
@@ -47,3 +112,4 @@ const process = (part, expectedAnswer, fn) => {
 }
 
 process('A', 136, partA)
+process('B', 64, partB)
